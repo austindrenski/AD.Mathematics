90ebe82b218ee41d3240c929a2c16da8435e7bdc
Austin Drenski <austin.drenski@gmail.com>
2017-09-08 21:37:33 -0400

    working on indicator creation

---
+++ AD.Mathematics.Tests/IndicatorTesting.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using AD.Mathematics.Matrix;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace AD.Mathematics.Tests
+{
+    [PublicAPI]
+    public static class IndicatorTesting
+    {
+        [Fact]
+        public static void TestMethod0()
+        {
+            double[][] data =
+                new double[][]
+                {
+                    new double[] { 1.0, 2.0, 3.0, 1.0 },
+                    new double[] { 1.0, 2.0, 3.0, 1.5 },
+                    new double[] { 1.0, 2.0, 3.0, 2.0 },
+                    new double[] { 1.0, 2.0, 3.0, 2.5 },
+                    new double[] { 1.0, 2.0, 3.0, 3.0 },
+                    new double[] { 1.0, 2.0, 3.0, 3.5 },
+                    new double[] { 1.0, 2.0, 3.0, 1.0 }
+                };
+
+            double[][] test = data.Indicate(3);
+            
+            Assert.Equal(data.Length, test.Length);
+            Assert.Equal(data[0].Length + 5, test[0].Length);
+            Assert.Equal(data.Select(x => x[3]).Distinct().Count(), 1);
+            Assert.Equal(data.Select(x => x[4]).Distinct().Count(), 1);
+            Assert.Equal(data.Select(x => x[5]).Distinct().Count(), 1);
+            Assert.Equal(data.Select(x => x[6]).Distinct().Count(), 1);
+            Assert.Equal(data.Select(x => x[7]).Distinct().Count(), 1);
+            Assert.Equal(data.Select(x => x[8]).Distinct().Count(), 1);
+        }
+        
+    }
+}
---
+++ AD.Mathematics/Matrix/Indicators.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace AD.Mathematics.Matrix
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [PublicAPI]
+    public static class Indicators
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="indicators"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static double[][] Indicate(this double[][] source, params int[] indicators) 
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            Dictionary<int, Dictionary<double, int>> indicatorLookup =
+                new Dictionary<int, Dictionary<double, int>>();
+
+            for (int i = 0; i < indicators.Length; i++)
+            {
+                SortedSet<double> set = new SortedSet<double>();
+                
+                Dictionary<double, int> indicatorMap = new Dictionary<double, int>();
+                
+                for (int j = 0; j < source.Length; j++)
+                {
+                    double value = source[j][indicators[i]];
+                    
+                    if (set.Add(value))
+                    {
+                        indicatorMap.Add(value, i);
+                    }
+                }
+                
+                indicatorLookup.Add(indicators[i], indicatorMap);
+            }
+
+            double[][] result = new double[source.Length][];
+
+            int sum = indicatorLookup.Sum(x => x.Value.Count);
+
+            int resultColumns = source.Length - indicators.Length + sum;
+            
+            for (int i = 0; i < source.Length; i++)
+            {
+                result[i] = new double[resultColumns];
+                
+                for (int j = 0; j < source[0].Length; j++)
+                {
+                    if (!indicators.Contains(j))
+                    {
+                        result[i][j] = source[i][j];
+                    }
+                    else
+                    {
+                        result[i][indicatorLookup[j][source[i][j]]] = 1.0;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
